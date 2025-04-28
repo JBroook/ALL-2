eb2bfe44f904783c56fbfcfd7cafe8a652593411
JBroook <JeremiahBoey@gmail.com>
2025-04-28 12:47:11 +0800

    Tried adding html files to website

--- ALLProject/ALLProject/settings.py
+++ ALLProject/ALLProject/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
