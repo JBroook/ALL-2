22fd8b0423625393fd9e9d0edd9e6186cbe7247d
WeirdOrange <ivanlai2005@gmail.com>
2025-06-07 21:45:12 +0800

    prep for printing feature

--- ALLProject/reports/views.py
+++ ALLProject/reports/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import render
 from django.conf import settings
 from django.db.models import F, Count, Sum
 from .models import Payment, Product, CartItem, Restock, Category
-from datetime import datetime
+from datetime import datetime,date
 
 # Create your views here.
 def ManagerReportView(request):
     template = loader.get_template('reports/partials/sales_transactions.html')
+    print(request.POST)
 
     # Get Best Selling Products
     all_products = Product.objects.all().values()
@@ -39,21 +40,28 @@ def ManagerReportView(request):
 
     # Sales made for each months within the year
     today = datetime.now()
+    this_month = today.strftime("%B").lower()
+
     all_monthly_sales = []
+    sales_per_month = []
+    
     total_yearly_sales = Payment.objects.filter(timeStamp__year = today.year).count()
-    print(today.month)
+    # print(today)
+    # print(sales_per_month)
 
     for i in range (1,13):
         monthly_sales = Payment.objects.filter(timeStamp__month = i).count()
         all_monthly_sales.append(monthly_sales)
+        sales_on_month = Payment.objects.filter(timeStamp__month = i).aggregate(revenue = Sum('total_cost'))
+        sales_per_month.append(sales_on_month['revenue'])
     
     for j in range(0,len(all_monthly_sales)):
         all_monthly_sales[j] = all_monthly_sales[j] / total_yearly_sales
 
-    for j in range(0,12):
-        print("Month: ",j+1)
-        print(all_monthly_sales[j])
-    print(all_monthly_sales)
+    # for j in range(0,12):
+    #     print("Month: ",j+1)
+    #     print(all_monthly_sales[j])
+    # print(sales_per_month)
 
     # Get Restock List
     all_restock = Restock.objects.select_related('product_id').values_list(
@@ -64,25 +72,36 @@ def ManagerReportView(request):
     #     print(items[1])
 
     # Get Today's Transactions
-    #print(today)
-    transacts_day = Payment.objects.filter(timeStamp__day = today.day).values()
-    transacts_mth = Payment.objects.filter(timeStamp__month = today.month).values()
+    total_revenue = 0
+    if 'change-monthly' in request.POST:
+        month_or_day = "month"
+        show_transacts = Payment.objects.filter(timeStamp__month = today.month).values()
+        for i in show_transacts:
+            total_revenue += float(i['total_cost'])
+    else:
+        month_or_day = "day"
+        show_transacts = Payment.objects.filter(timeStamp__day = today.day).values()
+        for i in show_transacts:
+            total_revenue += float(i['total_cost'])
+
+    print(total_revenue)
     # print(transacts_day)
     # for items in transacts_day:
     #     print(items)
-    # print("ah")
+    print(month_or_day)
     # print(transacts_mth)
     # for items in transacts_mth:
     #     print(items)
-
-    this_month = today.strftime("%B").lower()
     
     context = {
         'month': this_month,
         'best_product' : best_product,
         'category_sale' : category_sale,
         'all_monthly_sales' : all_monthly_sales,
+        'sales_per_month': sales_per_month,
         'all_restock' : all_restock,
-        'transacts' : transacts_day,
+        'transacts' : show_transacts,
+        'total_revenue' : total_revenue,
+        'month_or_day' : month_or_day,
         }
     return HttpResponse(template.render(context,request))
