f77dd4f986b58ee1d13718f81c23ec0311bc52b0
JBroook <JeremiahBoey@gmail.com>
2025-06-16 23:56:51 +0800

    Revert "Merge branch 'sidebar'"
    This reverts commit 742c9e49c1731d606bff7504ed1a797197905aa1, reversing
    changes made to c7517ec0cb7c96ff42491a42303379bcf07ec209.

--- ALLProject/users/context_processors.py
+++
@@ -1,7 +0,0 @@
-from users.models import Employee
-
-def user_role(request):
-    if request.user.is_authenticated:
-        role = Employee.objects.get(user=request.user).role
-        return {'user_role': role}
-    return {'user_role': None}
