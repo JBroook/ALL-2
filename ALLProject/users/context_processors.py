d177e7e6d650a208cc83df5e71b7b1582f573554
WeirdOrange <ivanlai2005@gmail.com>
2025-06-26 19:39:09 +0800

    Enhanced ui
    Changed Restock page
    Tweaked Navigation bar
    Added Hover to Product List

--- ALLProject/users/context_processors.py
+++ ALLProject/users/context_processors.py
@@ -3,9 +3,6 @@ from .models import Employee
 def user_role(request):
     if request.user.is_authenticated:
         try:
-            if request.user.is_staff:
-                pass
-            else:
                 return {'user_role': Employee.objects.get(user=request.user).role}
         except AttributeError:
             print("User Role: None, attribute error")
