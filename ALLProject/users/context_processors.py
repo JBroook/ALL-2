7f39bcdedbd5c838ad161ceb564cca5627b1b3b6
WeirdOrange <ivanlai2005@gmail.com>
2025-06-26 19:45:24 +0800

    removed unwanted cache

--- ALLProject/users/context_processors.py
+++ ALLProject/users/context_processors.py
@@ -3,7 +3,7 @@ from .models import Employee
 def user_role(request):
     if request.user.is_authenticated:
         try:
-                return {'user_role': Employee.objects.get(user=request.user).role}
+            return {'user_role': Employee.objects.get(user=request.user).role}
         except AttributeError:
             print("User Role: None, attribute error")
             return {'user_role': None}
